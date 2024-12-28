1ff83e2050cb2b7a7504979dc26b4d563a9609c6
Vitaliy <vitaliypolyanskiy@gmail.com>
2024-12-28 20:03:16 +0200

    Add project files.

---
+++ 8. ParallelFactorialAsync/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelFactorialAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayResultAsync(5);
+            DisplayResultAsync(6);
+            DisplayResultAsync(7);
+            Console.WriteLine("Введите строку: ");
+            Console.ReadLine();
+        }
+
+        static async void DisplayResultAsync(int num)
+        {
+            int result = await Factorial(num);
+            Console.WriteLine("\nФакториал числа {0} равен {1}", num, result);
+        }
+
+        static Task<int> Factorial(int x)
+        {
+            return Task.Run(() =>
+            {
+                int result = 1;
+                for (int i = 1; i <= x; i++)
+                {
+                    result *= i;
+                }
+                Thread.Sleep(5000);
+                return result;
+            });
+        }
+    }
+}
